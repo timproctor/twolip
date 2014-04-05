add2d17d1262445f7d193c82a697bb5cb10d2147
Timothy Proctor <timproctor@users.noreply.github.com>
2014-04-05 08:41:16 -0600

    Update 20140318205612_create_words.rb

--- db/migrate/20140318205612_create_words.rb
+++ db/migrate/20140318205612_create_words.rb
@@ -1,9 +1,13 @@
 class CreateWords < ActiveRecord::Migration
   def change
-    create_table :words do |t|
-      t.string :english, :afrikaans, :albanian, :croatian, :czech, :dutch, :esperanto, :finnish, :french, :german, :icelandic, :indonesian, :irish, :italian, :latin, :malay, :polish, :portuguese, :spanish, :turkish
+    create_table :words do |translate|
+      translate.string :english, :afrikaans, :albanian, :croatian,
+                       :czech, :dutch, :esperanto, :finnish, 
+                       :french, :german, :icelandic, :indonesian, 
+                       :irish, :italian, :latin, :malay,
+                       :polish, :portuguese, :spanish, :turkish
  
-      t.timestamps
+      translate.timestamps
     end
   end
 end
